c8ecf831812850f46470a930f929e8aef4ae9072
Dani Lyn <dlyn1110@gmail.com>
2019-01-24 02:44:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet){
-  let response;
-  if (feet < 400){
-    response = 'This one is on me!';
-  } else if (feet > 2000 && feet <= 2500){
-    response = 'I will gladly take thirty bucks.';
-  } else if (feet > 2500){
-    response = 'No can do.';
+    let response;
+    if (feet < 400) {
+      response = `This one is on me!`;
+    } else if (feet > 2000 && feet <= 2500) {
+      response = `I will gladly take your thirty bucks.`;
+    } else if (feet > 2500) {
+      response = `No can do.`;
+    }
+    return response;
   }
-  return response;
-}
